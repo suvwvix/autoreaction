5272fa9ec6fbfa4b1b77c8d6096b50373ee42442
suvwvix <78187985+suvwvix@users.noreply.github.com>
2024-01-10 19:23:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,149 @@
-const Discord = require("discord.js-selfbot-v13") // npm i discord.js-selfbot-v13 (required)
-const client = new Discord.Client({checkUpdate: false})
-const sphinx = require("sphinx-self");
-const keep_alive = require("./keep_alive.js");
+const Discord = require("discord.js-selfbot-v13");
+const client29 = new Discord.Client({ checkUpdate: false });
+const client30 = new Discord.Client({ checkUpdate: false });
+const client31 = new Discord.Client({ checkUpdate: false });
+const client32 = new Discord.Client({ checkUpdate: false });
+const client33 = new Discord.Client({ checkUpdate: false });
+const client34 = new Discord.Client({ checkUpdate: false });
+const client35 = new Discord.Client({ checkUpdate: false });
+const client36 = new Discord.Client({ checkUpdate: false });
+const client37 = new Discord.Client({ checkUpdate: false });
+const client38 = new Discord.Client({ checkUpdate: false });
+const client39 = new Discord.Client({ checkUpdate: false });
+const client40 = new Discord.Client({ checkUpdate: false });
+const client41 = new Discord.Client({ checkUpdate: false });
+const client42 = new Discord.Client({ checkUpdate: false });
+const client43 = new Discord.Client({ checkUpdate: false });
+const client44 = new Discord.Client({ checkUpdate: false });
+////
 
-new sphinx.Core(client).leveling({ channel: "1066775934960676994", randomLetters: true, type: 'ar', time: 3000 }) //hover for options
+const {userAccount} = require("sphinx-run");
 
-client.login(process.env.TOKEN) //Not saved.
+
+
+
+client31.on('ready', () => {
+  console.log(client31.user.tag)
+})
+new userAccount(client31, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client31.login("MTE1NTY0NTYzMDY2MDc1MTQ4MQ.GpKYR_.I04n_bXPdAXg7J2FJ-NUs5Z10YyqB8187KMzXE")
+
+client32.on('ready', () => {
+  console.log(client32.user.tag)
+})
+new userAccount(client32, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client32.login("MTE1NTY2Nzk4NDc4NjkxOTUyNQ.GX7jIT.gfDRg2ItG44Dk5vBq8puTRtFDzU-sdN2Ff2jkM")
+
+client33.on('ready', () => {
+  console.log(client33.user.tag)
+})
+new userAccount(client33, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client33.login("MTE1NjM5NzIzNDgzMzg1MDM4OQ.Ge0HlL.bCf3nz6J7Kxfb0Bx4jL345qv947XiItfAFQB_I")
+
+
+
+client35.on('ready', () => {
+  console.log(client35.user.tag)
+})
+new userAccount(client35, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client35.login("MTE1NTY2NDIxMjk5MTk0Njc4Mg.G4b8ka.x_zG2Z-QMz_esAIE1lAOUM_Ezis5-yJMklwe8c")
+
+client36.on('ready', () => {
+  console.log(client36.user.tag)
+})
+new userAccount(client36, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client36.login("MTE1NTY2NTMzNjQ1NzIzMjUwNA.GVQ0Si.tXxLgJUKPlx5gkQmoBW23nbFQXN0I1OGuqudWs")
+
+client37.on('ready', () => {
+  console.log(client37.user.tag)
+})
+new userAccount(client37, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client37.login("MTE1NTY2NzI3ODQ5NTQ5ODM2MQ.GHknRT.UaWlul0r3vEqtOUBcfeqEsjDyysRuPSvQrhU1w")
+
+
+client39.on('ready', () => {
+  console.log(client39.user.tag)
+})
+new userAccount(client39, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client39.login("MTE1NjAyNzA1Njg2NjQ3MTk2Ng.G49LA4.VtcIwZr7V_BLfMK7X9Y5D_Ri4Tx-PyqVn22Wcs")
+
+
+client40.on('ready', () => {
+  console.log(client40.user.tag)
+})
+new userAccount(client40, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client40.login("MTE1NjAzNjE3MzM1Mzk4ODE1Ng.GngO8r.3fIwsVnMxvQB9_vkBwQvprzcu64ngkyJt_r_LQ")
+
+
+client42.on('ready', () => {
+  console.log(client42.user.tag)
+})
+new userAccount(client42, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client42.login("MTE1NjQxNzUxNDE1NTI5ODg0Ng.G6S30E.c9GzuflX38uvb3foniq2hEdhoYm4gBsil26AV0")
+
+client43.on('ready', () => {
+  console.log(client43.user.tag)
+})
+new userAccount(client43, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client43.login("MTE1NjQyNDQ4MDAwNTAyOTk1OQ.GmMRls.JWS1iXsr3n6UVdP1dnVCeEBG4iIOTbe6cQmHPg")
+
+client44.on('ready', () => {
+  console.log(client44.user.tag)
+})
+new userAccount(client44, Discord).autoReaction({
+    channel: '1167824652450680932',
+    user: '574834734283358238',
+    customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1176966031143620660']
+});
+
+client44.login("MTE1NTY0OTI4NjM4MjU2MzMzOA.GRhmp5.CKnMj4OWI5NqdfEghXK4Sj5PTSGQOK8SoKDAp0")
